d871c1906da5fc1ae7b23a47722702262445b7cb
Juan Huertas <e4552601@gmail.com>
2026-02-12 00:43:31 -0500

    Forzar instalacion de dependencias en build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Salir si hay algún error
+set -o errexit
+
+# 1. FORZAR LA INSTALACIÓN DE LIBRERÍAS
+pip install -r requirements.txt
+
+# 2. Correr migraciones y estáticos
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
